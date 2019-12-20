cf023429dde9c355f7f4d4c363dcf0d33933f8ed
islasDC <35252769+islasDC@users.noreply.github.com>
2019-12-20 10:36:35 +0800

    show hide password
    toggle icon to show and hide password

--- admin/login.php
+++ admin/login.php
@@ -45,7 +45,10 @@
           <?php } ?>
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" name="pass" placeholder="Password" required="required" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
+            <!-- <input type="password" class="form-control" name="pass" placeholder="Password" required="required" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
+            <span toggle="#password-field" class="glyphicon glyphicon-eye-open field-icon toggle-password"></span> -->
+            <input id="password-field" type="password" class="form-control"  name="pass" placeholder="Password" required="required" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
+              <span toggle="#password-field" class="glyphicon glyphicon-eye-open field-icon toggle-password"></span>
         </div>
         <div class="form-group">
             <input type="submit" class="btn btn-warning btn-block" value="Log in" name="login">
@@ -101,3 +104,59 @@
   </div>
   
 </div>
+
+<style type="text/css">
+  .field-icon {
+  float: right;
+  margin-left: -30px;
+  margin-top: -25px;
+  position: relative;
+  z-index: 2;
+}
+
+.container{
+  padding-top:50px;
+  margin: auto;
+}
+</style>
+
+<!-- <div class="container">
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <form class="form-horizontal" method="" action="">
+
+          <div class="form-group">
+            <label class="col-md-4 control-label">Email</label>
+            <div class="col-md-6">
+              <input type="email" class="form-control" name="email" value="">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-md-4 control-label">Password</label>
+            <div class="col-md-6">
+              <input id="password-field" type="password" class="form-control" name="password" value="secret">
+              <span toggle="#password-field" class="glyphicon glyphicon-eye-open field-icon toggle-password"></span>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+</div> -->
+
+          <script type="text/javascript">
+            $(".toggle-password").click(function() {
+
+  $(this).toggleClass("glyphicon-eye-close");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+          </script>
